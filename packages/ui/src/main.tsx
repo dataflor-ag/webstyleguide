d265cd376d943dbb9cfd361579c8063363e91c14
Nikolai Wicke <n.wicke@flynet.de>
2024-03-06 14:20:59 +0100

    feat: clean up

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material"
+import theme from "./theme"
+
+// import App from "./App.tsx"
+import Example from "./sandbox/example.tsx"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <App /> */}
+      <Example />
+    </ThemeProvider>
+  </React.StrictMode>
 )
