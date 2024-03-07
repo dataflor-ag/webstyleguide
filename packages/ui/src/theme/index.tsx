acc60eecd42b27527d23e49432d95f56df5c4a76
Nikolai Wicke <n.wicke@flynet.de>
2024-03-07 11:58:34 +0100

    docs: create radius - rename spacing - create shape logic

--- packages/ui/src/theme/index.tsx
+++ packages/ui/src/theme/index.tsx
@@ -2,13 +2,15 @@ import { createTheme } from "@mui/material/styles"
 import palette from "./palette"
 import typography from "./typography"
 import components from "./components"
-import spacing from "./spacings"
+import spacing from "./spacing"
+import shape from "./shape"
 
 const theme = createTheme({
   palette,
   typography,
   components,
   spacing,
+  shape,
 })
 
 export default theme
