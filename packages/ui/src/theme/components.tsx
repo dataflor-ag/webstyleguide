7d490f977bf92708af18b57baba8da7f89aec448
Nikolai Wicke <n.wicke@flynet.de>
2024-03-18 13:35:33 +0100

    feat: adjust button sizing & disabled state

--- packages/ui/src/theme/components.tsx
+++ packages/ui/src/theme/components.tsx
@@ -1,4 +1,6 @@
 import { Components, Theme } from "@mui/material"
+import palette from "./palette"
+import theme from "./index"
 
 const components: Components<Omit<Theme, "components">> = {
   MuiGrid: {
@@ -24,8 +26,28 @@ const components: Components<Omit<Theme, "components">> = {
         fontWeight: "500",
         borderRadius: 8,
       }),
+      sizeSmall: () => ({
+        height: theme.spacing(8),
+        borderRadius: "0.375rem",
+        padding: theme.spacing(0, 4),
+      }),
+      sizeMedium: () => ({
+        height: theme.spacing(10),
+        borderRadius: "0.5rem",
+        padding: theme.spacing(0, 5),
+      }),
+      sizeLarge: () => ({
+        height: theme.spacing(12),
+        borderRadius: "0.625rem",
+        padding: theme.spacing(0, 6),
+      }),
       containedPrimary: {
         color: "white",
+        ":disabled": {
+          color: "white",
+          backgroundColor: `${palette.primary![500]}`,
+          opacity: 0.4,
+        },
       },
     },
   },
