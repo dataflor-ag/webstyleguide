9cb4836d0d090cdbc0bb9fe4bc86348454074f67
Nikolai Wicke <n.wicke@flynet.de>
2024-03-18 08:28:20 +0100

    feat: adjust vite.config.ts for build

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path"
+import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react-swc"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // react(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"],
+      name: "df-ui-theme",
+    },
+    // rollupOptions: {
+    //   external: ["react"],
+    //   output: {
+    //     globals: {
+    //       react: "react",
+    //     },
+    //   },
+    // },
+  },
 })
