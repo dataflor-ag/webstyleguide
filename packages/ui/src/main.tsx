dc249e3d6bde15ba2d2b2a2fe8287b43ee32edb4
Nikolai Wicke <n.wicke@flynet.de>
2024-03-18 08:57:04 +0100

    feat: cleanup

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -5,14 +5,12 @@ import { ThemeProvider } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
 import theme from "./theme"
 
-// import App from "./App.tsx"
 import Example from "./sandbox/example.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* <App /> */}
       <Example />
     </ThemeProvider>
   </React.StrictMode>
