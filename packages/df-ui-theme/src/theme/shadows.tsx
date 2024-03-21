1875cc7f1ce6b5d4ce2a78f0e54095a5ab85ef17
Nikolai Wicke <n.wicke@flynet.de>
2024-03-21 11:28:02 +0100

    feat: add shadow design token

---
+++ packages/df-ui-theme/src/theme/shadows.tsx
@@ -0,0 +1,31 @@
+import { Shadows } from "@mui/material"
+
+const shadows: Shadows = [
+  "none",
+  "0 1px 3px 0 rgba(0,0,0,0.08), 0 1px 2px -1px rgba(0,0,0,0.08), 0 0 0 1px #E9E9EC",
+  "0 2px 3px 0 rgba(0,0,0,0.08), 0 1px 2px -1px rgba(0,0,0,0.08), 0 0 0 1px #E9E9EC",
+  "0 4px 8px -2px rgba(0,0,0,0.10), 0 2px 4px 0 rgba(0,0,0,0.05), 0 0 0 1px #E9E9EC",
+  "0 12px 16px -4px rgba(0,0,0,0.10), 0 4px 6px -2px rgba(0,0,0,0.05), 0 0 0 1px #E9E9EC",
+  "0 8px 24px 0 rgba(0,0,0,0.08), 0 24px 48px -12px rgba(0,0,0,0.15), 0 0 0 1px #E9E9EC",
+  "0 24px 48px -12px rgba(0,0,0,0.15), 0 0 0 1px #E9E9EC",
+  "0 24px 48px -12px rgba(0,0,0,0.15), 0 0 0 1px #E9E9EC",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+  "none",
+]
+
+export default shadows
