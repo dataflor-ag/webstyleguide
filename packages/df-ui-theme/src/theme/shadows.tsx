65f248f54de029c2db81dea640275777127fb02d
m.merten <m.merten@flynet.de>
2024-03-21 15:19:13 +0100

    style: extend customShadows

--- packages/df-ui-theme/src/theme/shadows.tsx
+++ packages/df-ui-theme/src/theme/shadows.tsx
@@ -6,6 +6,10 @@ type CustomShadow = {
   focus: string
   error: string
   errorFocus: string
+  primary: string
+  primaryFocus: string
+  secondary: string
+  secondaryFocus: string
 }
 
 type CustomShadowNames = "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl"
@@ -14,6 +18,10 @@ const customShadowBase = `0 0 0 1px ${palette.grey?.[200]}`
 const customShadowFocus = `0 0 0 1px ${palette.grey?.[300]}, 0 0 0 4px ${palette.grey?.[200]}`
 const customShadowError = `0 0 0 1px ${palette.error?.[500]}`
 const customShadowErrorFocus = `0 0 0 1px ${palette.error?.[600]}, 0 0 0 4px ${palette.error?.[200]}`
+const customShadowPrimary = `0 0 0 1px ${palette.primary?.[600]}`
+const customShadowPrimaryFocus = `0 0 0 1px ${palette.primary?.[600]}, 0 0 0 4px ${palette.primary?.[100]}`
+const customShadowSecondary = `0 0 0 1px ${palette.grey?.[900]}`
+const customShadowSecondaryFocus = `0 0 0 1px ${palette.grey?.[900]}, 0 0 0 4px ${palette.grey?.[200]}`
 
 const customShadowXs = `0 1px 3px 0 rgba(0,0,0,0.08), 0 1px 2px -1px rgba(0,0,0,0.08)`
 const customShadowSm = `0 2px 3px 0 rgba(0,0,0,0.08), 0 1px 2px -1px rgba(0,0,0,0.08)`
@@ -29,42 +37,70 @@ export const customShadows: Record<CustomShadowNames, CustomShadow> = {
     focus: `${customShadowXs}, ${customShadowFocus}`,
     error: `${customShadowXs}, ${customShadowError}`,
     errorFocus: `${customShadowXs}, ${customShadowErrorFocus}`,
+    primary: `${customShadowXs}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadowXs}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadowXs}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadowXs}, ${customShadowSecondaryFocus}`,
   },
   sm: {
     base: `${customShadowSm}, ${customShadowBase}`,
     focus: `${customShadowSm}, ${customShadowFocus}`,
     error: `${customShadowSm}, ${customShadowError}`,
     errorFocus: `${customShadowSm}, ${customShadowErrorFocus}`,
+    primary: `${customShadowSm}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadowSm}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadowSm}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadowSm}, ${customShadowSecondaryFocus}`,
   },
   md: {
     base: `${customShadowMd}, ${customShadowBase}`,
     focus: `${customShadowMd}, ${customShadowFocus}`,
     error: `${customShadowMd}, ${customShadowError}`,
     errorFocus: `${customShadowMd}, ${customShadowErrorFocus}`,
+    primary: `${customShadowMd}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadowMd}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadowMd}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadowMd}, ${customShadowSecondaryFocus}`,
   },
   lg: {
     base: `${customShadowLg}, ${customShadowBase}`,
     focus: `${customShadowLg}, ${customShadowFocus}`,
     error: `${customShadowLg}, ${customShadowError}`,
     errorFocus: `${customShadowLg}, ${customShadowErrorFocus}`,
+    primary: `${customShadowLg}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadowLg}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadowLg}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadowLg}, ${customShadowSecondaryFocus}`,
   },
   xl: {
     base: `${customShadowXl}, ${customShadowBase}`,
     focus: `${customShadowXl}, ${customShadowFocus}`,
     error: `${customShadowXl}, ${customShadowError}`,
     errorFocus: `${customShadowXl}, ${customShadowErrorFocus}`,
+    primary: `${customShadowXl}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadowXl}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadowXl}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadowXl}, ${customShadowSecondaryFocus}`,
   },
   "2xl": {
     base: `${customShadow2xl}, ${customShadowBase}`,
     focus: `${customShadow2xl}, ${customShadowFocus}`,
     error: `${customShadow2xl}, ${customShadowError}`,
     errorFocus: `${customShadow2xl}, ${customShadowErrorFocus}`,
+    primary: `${customShadow2xl}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadow2xl}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadow2xl}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadow2xl}, ${customShadowSecondaryFocus}`,
   },
   "3xl": {
     base: `${customShadow3xl}, ${customShadowBase}`,
     focus: `${customShadow3xl}, ${customShadowFocus}`,
     error: `${customShadow3xl}, ${customShadowError}`,
     errorFocus: `${customShadow3xl}, ${customShadowErrorFocus}`,
+    primary: `${customShadow3xl}, ${customShadowPrimary}`,
+    primaryFocus: `${customShadow3xl}, ${customShadowPrimaryFocus}`,
+    secondary: `${customShadow3xl}, ${customShadowSecondary}`,
+    secondaryFocus: `${customShadow3xl}, ${customShadowSecondaryFocus}`,
   },
 }
 
