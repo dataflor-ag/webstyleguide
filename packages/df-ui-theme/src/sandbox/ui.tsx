4d39a064b6d796dd1f9bd0dc0e7d50154f8827af
m.merten <m.merten@flynet.de>
2024-03-25 08:50:58 +0100

    style: adjust position

--- packages/df-ui-theme/src/sandbox/ui.tsx
+++ packages/df-ui-theme/src/sandbox/ui.tsx
@@ -78,48 +78,6 @@ function App() {
       </Stack>
 
       <Typography variant="h2" mb={4} mt={8}>
-        Avatar
-      </Typography>
-      <Stack direction="row" spacing={2}>
-        <Avatar sx={{ width: 24, height: 24, fontSize: "0.625rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 32, height: 32, fontSize: "0.75rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 40, height: 40, fontSize: "1rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 48, height: 48, fontSize: "1.125rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 56, height: 56, fontSize: "1.25rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 64, height: 64, fontSize: "1.5rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 72, height: 72, fontSize: "1.5rem" }}>
-          NA
-        </Avatar>
-        <Avatar sx={{ width: 80, height: 80, fontSize: "1.5rem" }}>
-          NA
-        </Avatar>
-      </Stack>
-
-      <Typography variant="h2" mb={4} mt={8}>
-        Avatar group
-      </Typography>
-      <Stack direction="row">
-          <AvatarGroup total={8}>
-            <Avatar alt="Remy Sharp" src={AvatarImage} />
-            <Avatar alt="Travis Howard" src={AvatarImage} />
-            <Avatar alt="Agnes Walker" src={AvatarImage} />
-            <Avatar alt="Trevor Henderson" src={AvatarImage} />
-          </AvatarGroup>
-      </Stack>
-
-      <Typography variant="h2" mb={4} mt={8}>
         Input Fields
       </Typography>
       <Stack direction="column">
@@ -200,6 +158,48 @@ function App() {
             </CardContent>
           </CardActionArea>
         </Card>
+      </Stack>
+
+      <Typography variant="h2" mb={4} mt={8}>
+        Avatar
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Avatar sx={{ width: 24, height: 24, fontSize: "0.625rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 32, height: 32, fontSize: "0.75rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 40, height: 40, fontSize: "1rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 48, height: 48, fontSize: "1.125rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 56, height: 56, fontSize: "1.25rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 64, height: 64, fontSize: "1.5rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 72, height: 72, fontSize: "1.5rem" }}>
+          NA
+        </Avatar>
+        <Avatar sx={{ width: 80, height: 80, fontSize: "1.5rem" }}>
+          NA
+        </Avatar>
+      </Stack>
+
+      <Typography variant="h2" mb={4} mt={8}>
+        Avatar group
+      </Typography>
+      <Stack direction="row">
+          <AvatarGroup total={8}>
+            <Avatar alt="Remy Sharp" src={AvatarImage} />
+            <Avatar alt="Travis Howard" src={AvatarImage} />
+            <Avatar alt="Agnes Walker" src={AvatarImage} />
+            <Avatar alt="Trevor Henderson" src={AvatarImage} />
+          </AvatarGroup>
       </Stack>
     </Box>
   )
