4eca410faaf5ceafc79871ced4b8159cdb395b86
Nikolai Wicke <n.wicke@flynet.de>
2024-03-25 11:20:10 +0100

    feat: add inject css lib

--- packages/df-ui-empty-state/vite.config.ts
+++ packages/df-ui-empty-state/vite.config.ts
@@ -1,16 +1,16 @@
 import { join, resolve } from "node:path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import { libInjectCss } from "vite-plugin-lib-inject-css"
 import dts from "vite-plugin-dts"
 
 import { peerDependencies } from "./package.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), libInjectCss(), dts({ rollupTypes: true })],
   build: {
     target: "esnext",
-    minify: false,
     lib: {
       entry: resolve(__dirname, join("lib", "index.ts")),
       fileName: "index",
@@ -18,11 +18,6 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react/jsx-runtime", ...Object.keys(peerDependencies)],
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: "dashes",
     },
   },
 })
