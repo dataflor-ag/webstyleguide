c50b4510ba2d34dc96bb22017ab3a09b99c32f3b
Nikolai Wicke <n.wicke@flynet.de>
2024-03-25 10:39:29 +0100

    feat: create project & component

---
+++ packages/df-ui-empty-state/README.md
@@ -0,0 +1,9 @@
+# `@dataflor-ag/df-ui-empty-state`
+
+> The EmptyState component in React serves as a placeholder when there's no data to display. It enhances user experience by providing clear feedback. Developers can customize its appearance and behavior using props, making it adaptable for various applications.
+
+## Usage
+
+```
+import EmptyState from "@dataflor-ag/df-ui-empty-state";
+```
---
+++ packages/df-ui-empty-state/vite.config.ts
@@ -0,0 +1,28 @@
+import { join, resolve } from "node:path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import dts from "vite-plugin-dts"
+
+import { peerDependencies } from "./package.json"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ rollupTypes: true })],
+  build: {
+    target: "esnext",
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, join("lib", "index.ts")),
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react/jsx-runtime", ...Object.keys(peerDependencies)],
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "dashes",
+    },
+  },
+})
