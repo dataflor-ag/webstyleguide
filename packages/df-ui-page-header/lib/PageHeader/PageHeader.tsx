e20a50f4f569a181d44d61328e42f16fb2445ed8
Nikolai Wicke <n.wicke@flynet.de>
2024-03-27 08:56:00 +0100

    feat: create PageHeader

---
+++ packages/df-ui-page-header/lib/PageHeader/PageHeader.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Stack, Typography } from "@mui/material"
+import styled from "@emotion/styled"
+import ui from "@dataflor-ag/df-ui-theme"
+
+export interface PageHeaderProps {
+  title: string
+  description?: string
+  children?: React.ReactNode
+}
+
+const PageHeaderRoot = styled.div`
+  padding: 2rem 0;
+  border-bottom: 1px solid ${ui.theme.palette.grey[200]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const PageHeader = React.forwardRef<HTMLDivElement, PageHeaderProps>(
+  (props, ref) => (
+    <PageHeaderRoot ref={ref}>
+      <Stack gap={2}>
+        <Typography variant="h2">{props.title}</Typography>
+        {props.description && (
+          <Typography variant="body1">{props.description}</Typography>
+        )}
+      </Stack>
+      {props.children && <Stack gap={2}>{props.children}</Stack>}
+    </PageHeaderRoot>
+  )
+)
+
+export default PageHeader
