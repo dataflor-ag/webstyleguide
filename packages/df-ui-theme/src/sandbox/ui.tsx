044d55d12883816d948665c9b53d00a1820668f9
m.merten <m.merten@flynet.de>
2024-03-27 08:45:09 +0100

    style: add various link types and examples

--- packages/df-ui-theme/src/sandbox/ui.tsx
+++ packages/df-ui-theme/src/sandbox/ui.tsx
@@ -15,12 +15,14 @@ import {
   DialogTitle,
   AppBar,
   Container,
-  Toolbar
+  Toolbar,
+  Link
 } from "@mui/material"
 import Stack from "@mui/material/Stack"
 import Box from "@mui/material/Box"
 import Image from "../assets/contemplative-reptile.jpg"
 import AvatarImage from "../assets/avatar.jpg"
+import { grey } from '@mui/material/colors';
 
 
 function App() {
@@ -298,6 +300,47 @@ function App() {
           </Toolbar>
         </Container>
       </AppBar>
+
+      <Typography variant="h2" mb={4} mt={8}>
+        Links
+      </Typography>
+      <Typography variant="h3" mb={4} mt={8}>
+        Basic
+      </Typography>
+      <Stack direction="row" spacing={4}>
+        <Link href="#" underline="none">
+          {'underline="none"'}
+        </Link>
+        <Link href="#" underline="hover">
+          {'underline="hover"'}
+        </Link>
+        <Link href="#" underline="always">
+          {'underline="always"'}
+        </Link>
+      </Stack>
+
+      <Typography variant="h3" mb={4} mt={8}>
+        Color inherited
+      </Typography>
+      <Stack direction="row" spacing={4}>
+        <Link href="#" color="inherit" underline="none">
+          {'underline="none"'}
+        </Link>
+        <Link href="#" color="inherit" underline="hover">
+          {'underline="hover"'}
+        </Link>
+        <Link href="#" color="inherit" underline="always">
+          {'underline="always"'}
+        </Link>
+      </Stack>
+
+      <Typography variant="h3" mb={4} mt={8}>
+        In text
+      </Typography>
+      <Typography variant="body2">
+        Don’t have an account? <Link fontWeight={"600"} href="#" underline="none">Sign up</Link>
+      </Typography>
+
     </Box>
   )
 }
