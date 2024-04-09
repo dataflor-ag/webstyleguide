bcd779b55caf43e799f0f9c6800f2e0848eea052
Nikolai Wicke <n.wicke@flynet.de>
2024-04-09 08:58:20 +0200

    docs: optimize representation of the stories

--- stories/components/Drawer/DrawerSidebar.stories.tsx
+++ stories/components/Drawer/DrawerSidebar.stories.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import { Meta, StoryObj } from "@storybook/react"
 import { DrawerSidebar } from "../../../packages/df-ui-drawer/lib"
-import { Typography } from "@mui/material"
+import { Typography, Stack } from "@mui/material"
 
 const meta: Meta = {
   title: "Components/Drawer/DrawerSidebar",
   tags: ["autodocs"],
   component: DrawerSidebar,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: "calc(100% - 4.5rem)",
+          height: "200px",
+          position: "relative",
+          marginLeft: "4.5rem",
+          backgroundColor: "#e5e5e5",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 export default meta
@@ -22,11 +37,12 @@ export const Default: Story = {
 export const WithChildren: Story = {
   args: {
     children: (
-      <div>
-        <Typography variant="h2" mb={1}>
-          Hello World
-        </Typography>
-      </div>
+      <Stack gap={2}>
+        <Typography variant="h3">A</Typography>
+        <Typography variant="h3">B</Typography>
+        <Typography variant="h3">C</Typography>
+        <Typography variant="h3">D</Typography>
+      </Stack>
     ),
   },
 }
