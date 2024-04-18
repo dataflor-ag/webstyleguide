868a3a646a0fe8e0fc12af755490a32effdaf13f
m.merten <m.merten@flynet.de>
2024-04-18 17:34:07 +0200

    style: adjust table / data-grid appearance

--- packages/df-ui-theme/src/theme/components/dataGrid.ts
+++ packages/df-ui-theme/src/theme/components/dataGrid.ts
@@ -1,13 +1,87 @@
-import { Components, Theme } from "@mui/material"
+import { Components, Theme } from "@mui/material";
+import palette from "../palette";
+import { customShadows } from "../shadows";
 
 const Styles: Components<Omit<Theme, "components">> = {
   MuiDataGrid: {
+    defaultProps: {
+      columnHeaderHeight: 40,
+      rowHeight: 64,
+    },
     styleOverrides: {
       root: {
-        backgroundColor: "red",
+        "--DataGrid-rowBorderColor": palette.grey?.[200],
+        "--unstable_DataGrid-radius": "0.75rem",
+        border: "0 none",
+        boxShadow: customShadows.sm.base,
+        "*:focus, *:focus-within": {
+          outline: "none !important",
+        },
+      },
+      row: {
+        transition: "all 0.2s ease-in-out",
+        "&:hover": {
+          backgroundColor: palette.grey?.[50],
+        },
+        "&.Mui-selected": {
+          backgroundColor: palette.grey?.[100],
+          "&:hover": {
+            backgroundColor: palette.grey?.[100],
+          },
+        },
+      },
+      columnHeaders: {
+        "--DataGrid-containerBackground": palette.grey?.[50],
+      },
+      columnHeader: {
+        padding: "0 1.5rem",
+        height: "var(--DataGrid-headerHeight) !important",
+        color: palette.grey?.[600],
+        fontWeight: 500,
+        fontSize: "0.75rem",
+        position: "relative",
+        // "&:after": {
+        //   content: "''",
+        //   position: "absolute",
+        //   right: 0,
+        //   width: "1px",
+        //   height: "1rem",
+        //   backgroundColor: palette.grey?.[200],
+        // },
+      },
+      cell: {
+        padding: "0 1.5rem",
+        color: palette.grey?.[700],
+        "&[data-field='id']": {
+          fontWeight: 500,
+          color: palette.grey?.[900],
+          "&:before": {
+            content: "'#'",
+          },
+        },
+      },
+      cellCheckbox: {
+        maxWidth: "4rem !important",
+        width: "4rem !important",
+        minWidth: "4rem !important",
+        "+ *": {
+          paddingLeft: 0,
+        },
+      },
+      columnHeaderCheckbox: {
+        padding: "0",
+        maxWidth: "4rem !important",
+        width: "4rem !important",
+        minWidth: "4rem !important",
+        "+ *": {
+          paddingLeft: 0,
+        },
+      },
+      footerContainer: {
+        borderColor: "var(--DataGrid-rowBorderColor) !important",
       },
     },
   },
-}
+};
 
-export default Styles
+export default Styles;
--- stories/mui-x/DataGrid.stories.tsx
+++ stories/mui-x/DataGrid.stories.tsx
@@ -1,28 +1,69 @@
-import React from "react"
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid"
+import React from "react";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 
-import { Meta } from "@storybook/react"
+import { Meta } from "@storybook/react";
 
 const meta: Meta = {
   title: "MUI X/DataGrid",
-}
+};
 
-export default meta
+export default meta;
 
 export function _DataGrid() {
   const rows: GridRowsProp = [
-    { id: 1, col1: "Hello", col2: "World" },
-    { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-    { id: 3, col1: "MUI", col2: "is Amazing" },
-  ]
+    { id: 3061, lastName: "Snow", firstName: "Jon", age: 14 },
+    { id: 3062, lastName: "Lannister", firstName: "Cersei", age: 31 },
+    { id: 3063, lastName: "Lannister", firstName: "Jaime", age: 31 },
+    { id: 3064, lastName: "Stark", firstName: "Arya", age: 11 },
+    { id: 3065, lastName: "Targaryen", firstName: "Daenerys", age: 16 },
+    { id: 3066, lastName: "Melisandre", firstName: null, age: 172 },
+    { id: 3067, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 3068, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 3069, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
 
   const columns: GridColDef[] = [
-    { field: "col1", headerName: "Column 1", width: 150 },
-    { field: "col2", headerName: "Column 2", width: 150 },
-  ]
+    { field: "id", headerName: "ID", width: 124 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 172,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 172,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 132,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 196,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ];
   return (
-    <div style={{ height: 300, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
-    </div>
-  )
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      checkboxSelection
+      disableRowSelectionOnClick
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 5,
+          },
+        },
+      }}
+      pageSizeOptions={[5]}
+    />
+  );
 }
