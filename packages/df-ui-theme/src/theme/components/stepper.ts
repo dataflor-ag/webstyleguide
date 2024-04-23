56a8207cfe2fe22c4ac58aad7c3c15f2198aec46
Nikolai Wicke <n.wicke@flynet.de>
2024-04-23 10:00:24 +0200

    style: colors, fonts & alignments

---
+++ packages/df-ui-theme/src/theme/components/stepper.ts
@@ -0,0 +1,83 @@
+import { Components, Theme } from "@mui/material"
+import theme, { palette } from "../index"
+
+// TODO: Own icon
+// TODO: Cant use custom icon
+// import { radioButtonIcon } from "../utils/radioButton"
+// import { checkboxIcon } from "../utils/checkbox"
+
+const Styles: Components<Omit<Theme, "components">> = {
+  MuiStepper: {
+    defaultProps: {},
+    styleOverrides: {
+      root: () => ({
+        // backgroundColor: "purple",
+      }),
+    },
+  },
+  MuiStepLabel: {
+    defaultProps: {
+      // StepIconComponent: radioButtonIcon,
+    },
+    styleOverrides: {
+      root: () => ({
+        // backgroundColor: "pink",
+        // color: "pink",
+        "&.Mui-active": {
+          backgroundColor: "blue",
+        },
+      }),
+      label: () => ({
+        color: theme.palette.grey?.[500],
+        "&.Mui-active": {
+          color: theme.palette.grey?.[500],
+        },
+      }),
+      iconContainer: {
+        // backgroundColor: "orange",
+      },
+    },
+  },
+  MuiStepConnector: {
+    styleOverrides: {
+      line: () => ({
+        borderColor: theme.palette.grey?.[200],
+      }),
+    },
+  },
+  MuiStepIcon: {
+    defaultProps: {
+      // icon: checkboxIcon,
+      // icon: checkboxIcon,
+    },
+    styleOverrides: {
+      root: () => ({
+        color: "white",
+        border: `1px solid ${palette.grey?.[200]}`,
+        borderRadius: "50%",
+        "&.Mui-active": {
+          color: `${palette.grey?.[900]}`,
+
+          "& .MuiStepIcon-text": {
+            fill: "white",
+          },
+        },
+        "&.Mui-completed": {
+          color: `${palette.success?.[600]}`,
+        },
+      }),
+      text: () => ({
+        fill: `${palette.grey?.[500]}`,
+        fontWeight: "600",
+      }),
+      active: () => ({
+        backgroundColor: "pink",
+      }),
+      completed: () => ({
+        backgroundColor: "yellow",
+      }),
+    },
+  },
+}
+
+export default Styles
