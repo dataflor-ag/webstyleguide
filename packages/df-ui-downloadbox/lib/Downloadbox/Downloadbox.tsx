51d8f2872eebc1f4c7e976d5f1c4c275eb91083a
Nikolai Wicke <n.wicke@flynet.de>
2024-05-13 10:38:57 +0200

    style: optimize sizings

--- packages/df-ui-downloadbox/lib/Downloadbox/Downloadbox.tsx
+++ packages/df-ui-downloadbox/lib/Downloadbox/Downloadbox.tsx
@@ -56,7 +56,7 @@ const DownloadboxIconText = styled("div")<DownloadboxIconTextProps>(
     left: 0,
     height: "1rem",
     lineHeight: "1rem",
-    padding: "0 0.5rem",
+    padding: "0 0.25rem",
   },
   (props) => ({
     color: colorMapping(props.mimeType).color,
@@ -98,16 +98,15 @@ export const Downloadbox = React.forwardRef<HTMLDivElement, DrawerFooterProps>(
       <DownloadboxRoot ref={ref}>
         <DownloadboxIcon>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="40">
-            <g fill="none" fill-rule="evenodd">
+            <g fill="none" fillRule="evenodd">
               <path
                 stroke="#E9E9EC"
-                stroke-linejoin="square"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 d="M16.515.75a5.25 5.25 0 0 1 3.712 1.538l9.485 9.485a5.25 5.25 0 0 1 1.538 3.712V34c0 1.45-.588 2.762-1.538 3.712A5.234 5.234 0 0 1 26 39.25H6a5.234 5.234 0 0 1-3.712-1.538A5.234 5.234 0 0 1 .75 34V6c0-1.45.588-2.762 1.538-3.712A5.234 5.234 0 0 1 6 .75Z"
               />
               <path
                 stroke="#E9E9EC"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 d="M31 13h-8a4 4 0 0 1-4-4V1"
               />
             </g>
