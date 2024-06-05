fec68e7fde13890aa4f26ad450e54a1d0d964f71
Nikolai Wicke <n.wicke@flynet.de>
2024-06-05 07:46:29 +0200

    feat: add pro date  & timepicker pro version

--- packages/df-ui-theme/src/theme.tsx
+++ packages/df-ui-theme/src/theme.tsx
@@ -7,6 +7,7 @@ import type {} from '@mui/x-data-grid-pro/themeAugmentation'
 import type {} from "@mui/x-tree-view/themeAugmentation"
 import type {} from "@mui/lab/themeAugmentation"
 import type {} from '@mui/x-date-pickers/themeAugmentation'
+import type {} from '@mui/x-date-pickers-pro/themeAugmentation'
 
 import { getComponents } from "./utils/getComponents"
 import { getPalette } from "./utils/getPalette"
