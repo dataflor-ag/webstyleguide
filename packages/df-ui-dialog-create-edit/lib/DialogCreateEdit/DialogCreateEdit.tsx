c6ee8323fedc30012574d9705312776bb0145bb3
wiebken-d-df <nagel@dataflor.de>
2024-07-08 14:29:13 +0200

    [new-component-test] added create edit dialog

---
+++ packages/df-ui-dialog-create-edit/lib/DialogCreateEdit/DialogCreateEdit.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import { styled } from "@mui/material/styles"
+import { Typography, IconButton, Button, Dialog, DialogActions, DialogContent, DialogTitle, Box, type Breakpoint} from "@mui/material"
+import Icon from "@dataflor-ag/df-ui-icons"
+
+type IconVariant = "default" | "primary" | "success" | "warning" | "error" | "info"
+
+interface DialogProps {
+  title: string
+  subtitle?: string
+  icon?: React.ReactNode;
+  open: boolean;
+  children?: React.ReactNode;
+  content?: React.ReactNode
+  onClose?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  onSave?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  onEdit?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  buttonTextClose?: string,
+  buttonTextSave?: string,
+  maxWidth?: Breakpoint
+}
+
+const DialogCreateEditRoot = styled("div", {
+  name: "MuiDialog",
+  slot: "root",
+})(({ theme, }) => ({
+  width: "100%",
+  display: "flex",
+  gap: theme.spacing(4),
+  alignItems: "center",
+  padding: theme.spacing(5),
+  borderBottom: `1px solid ${theme.palette.grey[200]}`,
+}))
+
+
+
+export const DialogCreateEdit = React.forwardRef<HTMLDivElement, DialogProps>(
+  (props, ref) => {
+
+    const { icon, title, onClose, onSave, open, children, buttonTextClose, buttonTextSave, maxWidth} = props
+    const handleClose = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation()
+      if(onClose) onClose(e)
+    }
+
+    return (
+      <DialogCreateEditRoot ref={ref}>
+        <Dialog open={open} maxWidth={maxWidth ? maxWidth : "xs"}>
+          <DialogTitle>
+            <Box
+            display={"flex"}
+            alignItems={"center"}
+            gap={3}
+            sx={{
+              "& .MuiSvgIcon-fontSizeMedium": {
+                fontSize: "20px",    
+            }}}>
+            {icon}
+            <Typography variant="h6">{title}</Typography>
+            </Box> 
+          </DialogTitle>
+          <DialogContent dividers>{children}
+          </DialogContent> 
+          <DialogActions>
+          <Button color="secondary" variant="outlined" onClick={onClose}>{buttonTextClose}
+          </Button>
+            <Button color="primary" onClick={onSave}>{buttonTextSave}
+            </Button>
+          </DialogActions>  
+        </Dialog>
+      </DialogCreateEditRoot>
+    )
+  }
+)
---
+++ stories/components/DialogCreateEdit.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { Meta, StoryObj } from "@storybook/react"
+import { DialogCreateEdit } from "../../packages/df-ui-dialog-create-edit/lib"
+import { Box, Button , Typography } from "@mui/material"
+import Icon from "@dataflor-ag/df-ui-icons"
+
+const meta: Meta<typeof DialogCreateEdit> = {
+  title: "Components/DialogCreateEdit",
+  component: DialogCreateEdit,
+  tags: ["autodocs"],
+  argTypes: {
+    title: {
+      type: "string",
+    },
+    subtitle: {
+      type: "string",
+    },
+   
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof DialogCreateEdit>
+
+export function DialogExample() {
+  const [open, setOpen] = React.useState(false)
+  const toggleDialog =  (newOpen: boolean) => () => {
+    setOpen(newOpen)
+  }
+  function onSave() {
+    console.log("save")
+  }
+  function onClose() {
+    console.log("close")
+    setOpen(false)
+  }
+  return (
+  <Box sx={{ width: "100%", height: "400px" }} role="presentation" p={4}>
+    <Button onClick={toggleDialog(true)}>Open Dialog</Button>
+    <DialogCreateEdit
+      onSave={onSave}
+      onClose={onClose}
+      open={open}
+      title="title"
+      buttonTextClose={"buttonTextClose"}
+      buttonTextSave={"buttonTextSave"}
+      icon={<Icon.documentEdit/>
+      }>
+      <Box height="20rem" sx={{backgroundColor: "#E6E2D6", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <Typography variant="h4" >Child Content</Typography>
+      </Box>
+    </DialogCreateEdit>
+  </Box>
+)
+}
+
