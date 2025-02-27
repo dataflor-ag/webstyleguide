9d656bcb918b4d15567c06d9b9d89825e3f962da
Mohammed-Amine Waoua <am.wawa42@gmail.com>
2025-02-27 14:25:27 +0100

    Upgrade Storybook to version 8.6.2
    Update Storybook and related dependencies to the latest minor version, simplifying the Storybook configuration by removing the getAbsolutePath function

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,3 @@
-import { join, dirname } from "path"
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")))
-}
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
@@ -15,22 +5,22 @@ const config = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
     {
-      name: getAbsolutePath("@storybook/addon-storysource"),
+      name: "@storybook/addon-storysource",
       options: {
         loaderOptions: {
           injectStoryParameters: false,
         },
       },
     },
-    getAbsolutePath("@storybook/addon-themes"),
-    // getAbsolutePath("@storybook/addon-onboarding"),
-    // getAbsolutePath("@storybook/addon-interactions"),
+    "@storybook/addon-themes",
+    // "@storybook/addon-onboarding",
+    // "@storybook/addon-interactions",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
   typescript: {
